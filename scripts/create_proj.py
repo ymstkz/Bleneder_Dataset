90fad13528d0028dbea2ef84e923d4ccdfd37ad4
ymstkz <yamashita19990304@gmail.com>
2022-10-10 03:51:13 +0900

    more noisy

--- scripts/create_proj.py
+++ scripts/create_proj.py
@@ -12,14 +12,13 @@ from math import pi
 from mathutils import Vector
 import datetime
 import cv2
-import time
 
 ###################################
 ### set parameters 
 ###################################
 
-target_model = "traffic_light_red"
-num_of_photo = 0
+target_model       = "traffic_light_red"
+num_of_photo       = 10
 background_img_dir = "/media/slab/LAP_CHOL_DATA_2/contest_dataset/background/"
 output_dir         = "/media/slab/LAP_CHOL_DATA_2/contest_dataset/ymstkz/"
 dt                 = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
@@ -55,8 +54,6 @@ def label(scene, obj, i):
     )
     left_up = [round(max(bound_box[0]) * render_size[0]), round(min(bound_box[1]) * render_size[1])]
     right_bottom = [round(min(bound_box[0]) * render_size[0]), round(max(bound_box[1]) * render_size[1])]
-    print("Left Up Coords:", left_up)
-    print("Rgiht Buttom Coords:", right_bottom)
     
     return [left_up, right_bottom]
 
@@ -64,18 +61,24 @@ def random_setting(target):
     scale = random.uniform(0.5, 1.0)
     location = [random.uniform(-0.5, 0.5), random.uniform(-0.5, 0.5)]
     rotate = [random.uniform(-0.5, 0.5), random.uniform(-0.5, 0.5)]
-    bpy.data.objects["traffic_light_red"].scale[0]            = scale
-    bpy.data.objects["traffic_light_red"].scale[1]            = scale
-    bpy.data.objects["traffic_light_red"].scale[2]            = scale
-    bpy.data.objects["traffic_light_red"].location[0]         = location[0]
-    bpy.data.objects["traffic_light_red"].location[2]         = location[1]
-    bpy.data.objects["traffic_light_red"].rotation_euler[1]   = rotate[1]
-    bpy.data.objects["traffic_light_red"].rotation_euler[2]   = rotate[0]
+    target.scale[0]            = scale
+    target.scale[1]            = scale
+    target.scale[2]            = scale
+    target.location[0]         = location[0]
+    target.location[2]         = location[1]
+    target.rotation_euler[1]   = rotate[1]
+    target.rotation_euler[2]   = rotate[0]
 
-    # lighting
-    bpy.data.materials["traffic_light_red"].node_tree.nodes["Principled BSDF"].inputs[20].default_value = random.uniform(0, 20) # housha
-    bpy.data.materials["traffic_light_red"].node_tree.nodes["カラーランプ"].color_ramp.elements[1].color = (1, 0.0192237, 0, 1) # RGBA
-    bpy.data.materials["traffic_light_red"].node_tree.nodes["カラーランプ"].color_ramp.elements[1].color = (1, 0.0192237, 0, 1) # RGBA
+    if target_model == "traffic_light_red":
+        # lighting
+        bpy.data.materials[target_model].node_tree.nodes["Principled BSDF"].inputs[20].default_value = random.uniform(0, 20) # housha
+        bpy.data.materials[target_model].node_tree.nodes["カラーランプ"].color_ramp.elements[1].color = (random.uniform(0, 1),random.uniform(0, 0.05), 0, 1)
+        bpy.data.worlds["World"].node_tree.nodes["Background"].inputs[1].default_value = random.uniform(1, 5)
+        rand = random.uniform(0.2, 1)
+        bpy.data.materials["traffic_light_body"].node_tree.nodes["RGB.002"].outputs[0].default_value = (rand, rand, rand, 1)
+        rand = random.uniform(0.2, 1)
+        bpy.data.materials["traffic_light_body"].node_tree.nodes["Principled BSDF.001"].inputs[3].default_value = (rand, rand, rand, 1)
+
 
 def main():
     if not os.path.exists(background_img_dir):
@@ -96,7 +99,9 @@ def main():
         
     os.mkdir(out_dir)   
     os.mkdir(out_label_dir)
-    os.mkdir(out_image_dir)    
+    os.mkdir(out_image_dir)
+
+    flabel = open(out_label_dir + "label.txt", 'w', encoding='UTF-8')
     
     # create project
     bpy.data.objects.remove(bpy.data.objects["Cube"])
@@ -105,7 +110,7 @@ def main():
 
     # add camera
     camera = bpy.data.objects["Camera"]
-    camera.location = (0, -3, 0)
+    camera.location = (0, -4, 0)
     camera.rotation_euler = (((pi * 90 / 180),0,0))
     scene.render.resolution_x = resolution_x
     scene.render.resolution_y = resolution_y
@@ -118,6 +123,8 @@ def main():
         scene.render.engine = 'CYCLES'
         scene.cycles.device = 'GPU'
         scene.render.film_transparent = True
+        scene.cycles.samples = 2048
+
     
     scene.use_nodes = True
     node_tree = scene.node_tree
@@ -150,10 +157,17 @@ def main():
     
     back_image_node.image = bpy.data.images.load(background_img_dir  + str(0).zfill(digit) + ".jpg", check_existing=False)
     env_node.image = bpy.data.images.load(background_img_dir + str(0).zfill(digit) + ".jpg", check_existing=False)
-    target = bpy.data.objects["traffic_light_body"]
+    
+    target = bpy.data.objects[target_model]
 
     # start create dataset
-    for i in range(0, num_of_photo):
+    i = 0
+    while 1:
+        
+        if i >= num_of_photo:
+            break
+        print(f"\rprogress {i} / {num_of_photo}")
+
         back_image_node.image = bpy.data.images.load(background_img_dir  + str(i).zfill(digit) + ".jpg", check_existing=False)
         env_node.image = bpy.data.images.load(background_img_dir + str(i).zfill(digit) + ".jpg", check_existing=False)
         
@@ -162,16 +176,12 @@ def main():
         cood = label(scene, target, int(i))
 
         if cood[0][0] > resolution_x or cood[0][0] < 0: 
-            i-=1
             continue
         if cood[1][0] > resolution_x or cood[1][0] < 0:
-            i-=1
             continue 
         if cood[0][1] > resolution_y or cood[0][1] < 0:
-            i-=1
             continue
         if cood[1][1] > resolution_y or cood[1][1] < 0: 
-            i-=1
             continue
 
         if debug:
@@ -180,6 +190,10 @@ def main():
             img = cv2.circle(img, (cood[0][0], resolution_y-cood[0][1]), 10, (0, 255, 0))
             img = cv2.circle(img, (cood[1][0], resolution_y-cood[1][1]), 10, (0, 255, 0))
             cv2.imwrite(filename="./out.png", img=img)
+        flabel.write(f'{cood[0][0]},{resolution_y-cood[0][1]},{cood[1][0]},{resolution_y-cood[1][1]}\n')
+        i+=1
+
+    flabel.close()
 
 if __name__  == "__main__":
     main()
