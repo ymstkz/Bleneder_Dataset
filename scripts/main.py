0c7bcaa706abf9c02dafe9015438a9cbc1b8d695
ymstkz <yamashita19990304@gmail.com>
2022-11-27 14:38:02 +0900

    add latest models and script

--- scripts/main.py
+++ scripts/main.py
@@ -14,17 +14,25 @@ import datetime
 import cv2
 import colorsys
 import time
+import mathutils
 
 ###################################
 ### set parameters 
 ###################################
-target_model       = "traffic_light_green"
-num_of_photo       = 50000
+target_model       = "traffic_light_red"
+model_number       = 1
+
+if target_model == "doll":
+    blender_file   = target_model + "_" + str(model_number)
+else:
+    blender_file   = target_model
+
+num_of_photo       = 30000
 dataset_offset     = 0
 
 
-background_img_dir = "/media/ymstkz/LAP_CHOL_DATA_2/contest_dataset/background/car/"
-output_dir         = "/media/ymstkz/LAP_CHOL_DATA_2/contest_dataset/ymstkz/"
+background_img_dir = "/media/slab/LAP_CHOL_DATA_2/contest_dataset/background/car/"
+output_dir         = "/media/slab/LAP_CHOL_DATA_2/contest_dataset/ymstkz/"
 dt                 = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
 out_dir            = output_dir + target_model + "/" + dt + "/"
 out_label_dir      = out_dir + "labels/"
@@ -32,18 +40,18 @@ out_image_dir      = out_dir + "images/"
 out_debug_dir      = out_dir + "debugs/"
 digit              = 10
 use_gpu            = True
-debug              = False
-resolution_x       = 1280
-resolution_y       = 720
+debug              = True
+resolution_x       = 720
+resolution_y       = 1280
 
 clss = {
     'traffic_light_red'    : 0, 
     'traffic_light_yellow' : 1, 
     'traffic_light_green'  : 2,
-    'arrow'                : 3,
-    'barricade'            : 4,
-    'bar'                  : 5,
-    'doll'                 : 6
+    'doll'                 : 3,
+    'arrow'                : 4,
+    'barricade'            : 5,
+    'bar'                  : 6,
     }
 
 # save renderrig image
@@ -92,11 +100,11 @@ def convert(size, box):
 def random_setting(target):
 
     # environment lighting 
-    bpy.data.worlds["World"].node_tree.nodes["Background"].inputs[1].default_value = random.uniform(1, 3)
+    bpy.data.worlds["World"].node_tree.nodes["Background"].inputs[1].default_value = random.uniform(3, 5)
     
     # lighting
     if target_model == "traffic_light_red":
-        scale = random.uniform(0.2, 1.0)
+        scale = random.uniform(0.3, 0.4)
         location = [random.uniform(-0.4, 1.0), random.uniform(-0.5, 0.5)]
         rotate = [random.uniform(-0.6, 0.1), 0, random.uniform(-0.3, 0.3)]
         target.scale[0]            = scale
@@ -108,7 +116,7 @@ def random_setting(target):
         target.rotation_euler[1]   = rotate[1]
         target.rotation_euler[2]   = rotate[2]
         bpy.data.materials[target_model].node_tree.nodes["Principled BSDF"].inputs[20].default_value = random.uniform(0.5, 10)
-        hue = random.uniform(0.95,1) if random.randint(0,1) else random.uniform(0,0.05)
+        hue = random.uniform(0.95,1) if random.randint(0,1) else random.uniform(0,0.01)
         rgb = colorsys.hsv_to_rgb(hue, random.uniform(0,1), 1)
         bpy.data.materials[target_model].node_tree.nodes["カラーランプ"].color_ramp.elements[1].color = (rgb[0], rgb[1], rgb[2], 1)
         
@@ -119,8 +127,8 @@ def random_setting(target):
         bpy.data.materials["traffic_light_body"].node_tree.nodes["Principled BSDF.001"].inputs[3].default_value = (rand, rand, rand, 1)
     
     if target_model == "traffic_light_yellow":
-        scale = random.uniform(0.3, 1.1)
-        location = [random.uniform(-0.6, 0.6), random.uniform(-0.5, 0.5)]
+        scale = random.uniform(0.2, 1.0)
+        location = [random.uniform(-1.0, 1.0), random.uniform(-0.4, 0.4)]
         rotate = [random.uniform(-0.6, 0.1), 0, random.uniform(-0.3, 0.3)]
         target.scale[0]            = scale
         target.scale[1]            = scale
@@ -130,18 +138,22 @@ def random_setting(target):
         target.rotation_euler[0]   = rotate[0]
         target.rotation_euler[1]   = rotate[1]
         target.rotation_euler[2]   = rotate[2]
-        bpy.data.materials[target_model].node_tree.nodes["Principled BSDF"].inputs[20].default_value = random.uniform(0.5, 15)
-        hue = random.uniform(0.95,1) if random.randint(0,1) else random.uniform(0,0.05)
-        rgb = colorsys.hsv_to_rgb(hue, random.uniform(0,1), 1)
+        bpy.data.materials[target_model].node_tree.nodes["Principled BSDF"].inputs[20].default_value = random.uniform(1, 12)
+        hue = random.uniform(0.025,0.10)
+        saturation = random.uniform(0.95, 1)
+        rgb = colorsys.hsv_to_rgb(hue, saturation, 1)
+        bpy.data.materials[target_model].node_tree.nodes["カラーランプ"].color_ramp.elements[0].color = (rgb[0], rgb[1], rgb[2], 1)
+        rgb = colorsys.hsv_to_rgb(hue, random.uniform(saturation-0.1,saturation), 1)
         bpy.data.materials[target_model].node_tree.nodes["カラーランプ"].color_ramp.elements[1].color = (rgb[0], rgb[1], rgb[2], 1)
-        bpy.data.materials["traffic_light_body"].node_tree.nodes["Principled BSDF.001"].inputs[7].default_value = random.uniform(0, 0.3)
+        bpy.data.materials["traffic_light_body"].node_tree.nodes["Principled BSDF.001"].inputs[7].default_value = random.uniform(0, 0.2)
         rand = random.uniform(0.1, 0.9)
         bpy.data.materials["traffic_light_body"].node_tree.nodes["RGB.002"].outputs[0].default_value = (rand, rand, rand, 1)
         rand = random.uniform(0.2, 0.9)
         bpy.data.materials["traffic_light_body"].node_tree.nodes["Principled BSDF.001"].inputs[3].default_value = (rand, rand, rand, 1)
-    
+
+   
     if target_model == "traffic_light_green":
-        scale = random.uniform(0.3, 1.1)
+        scale = random.uniform(0.2, 1.0)
         location = [random.uniform(-1.0, 0.4), random.uniform(-0.4, 0.4)]
         rotate = [random.uniform(-0.6, 0.1), 0, random.uniform(-0.3, 0.3)]
         target.scale[0]            = scale
@@ -164,6 +176,55 @@ def random_setting(target):
         bpy.data.materials["traffic_light_body"].node_tree.nodes["RGB.002"].outputs[0].default_value = (rand, rand, rand, 1)
         rand = random.uniform(0.2, 0.9)
         bpy.data.materials["traffic_light_body"].node_tree.nodes["Principled BSDF.001"].inputs[3].default_value = (rand, rand, rand, 1)
+
+    if target_model == "doll":
+        body = bpy.context.scene.objects["doll_body"]
+        bpy.context.view_layer.objects.active = body
+        bpy.ops.object.mode_set(mode='POSE')
+        # reset positon and scale
+        body.scale          = [1, 1, 1]
+        body.location       = [0, 0, 0]
+        body.rotation_euler = [0, 0, 0]
+
+        if model_number == 0:
+            # move arms
+            body.pose.bones["arm_r_ik"].location     = (random.uniform(-1.4, 0.01), random.uniform(-0.7, 0.7), random.uniform(-0.65, 0.02))
+            body.pose.bones["arm_l_ik"].location     = (random.uniform(-1.4, 0.01), random.uniform(-0.7, 0.7), random.uniform(-0.02, 0.65))
+            body.pose.bones["hand_r"].rotation_mode  = 'XYZ'
+            body.pose.bones["hand_r"].rotation_euler = mathutils.Euler((random.uniform(-40, 40), random.uniform(-90, 50),0), 'XYZ')
+            body.pose.bones["hand_l"].rotation_mode  = 'XYZ'
+            body.pose.bones["hand_l"].rotation_euler = mathutils.Euler((random.uniform(-40, 40), random.uniform(-90, 50),0), 'XYZ')
+            # move f
+            body.pose.bones["toes_r_ik"].location    = (random.uniform(-0.6, 0), random.uniform(-0.75, 0.75), random.uniform(-0.07, 0.07))
+            body.pose.bones["toes_l_ik"].location    = (random.uniform(-0.6, 0), random.uniform(-0.75, 0.75), random.uniform(-0.07, 0.07))
+            #
+            body.pose.bones["head"].rotation_mode    = 'XYZ'
+            body.pose.bones["head"].rotation_euler   = mathutils.Euler((random.uniform(-10, 10), random.uniform(-180, 180), random.uniform(-10, 10)), 'XYZ')
+            body.pose.bones["chest"].rotation_mode   = 'XYZ'
+            body.pose.bones["chest"].rotation_euler  = mathutils.Euler((random.uniform(-10, 10), random.uniform(-10, 10), random.uniform(-10, 10)), 'XYZ')
+        elif model_number == 1:
+            # move
+            body.pose.bones["arm_r_ik"].location     = (random.uniform(-0.55, 0.55), random.uniform(-0.55, 0.55), random.uniform(-0.0025, 0.47))
+            body.pose.bones["arm_l_ik"].location     = (random.uniform(-0.55, 0.55), random.uniform(-0.55, 0.55), random.uniform(-0.47, 0.0025))
+            body.pose.bones["hand_r"].rotation_mode  = 'XYZ'
+            body.pose.bones["hand_r"].rotation_euler = mathutils.Euler((random.uniform(-40, 40), random.uniform(-90, 50),0), 'XYZ')
+            body.pose.bones["hand_l"].rotation_mode  = 'XYZ'
+            body.pose.bones["hand_l"].rotation_euler = mathutils.Euler((random.uniform(-40, 40), random.uniform(-90, 50),0), 'XYZ')
+            # move f
+            body.pose.bones["toes_r_ik"].location    = (random.uniform(-0.6, 0), random.uniform(-0.75, 0.75), random.uniform(-0.07, 0.07))
+            body.pose.bones["toes_l_ik"].location    = (random.uniform(-0.6, 0), random.uniform(-0.75, 0.75), random.uniform(-0.07, 0.07))
+            #
+            body.pose.bones["head"].rotation_mode    = 'XYZ'
+            body.pose.bones["head"].rotation_euler   = mathutils.Euler((random.uniform(-10, 10), random.uniform(-180, 180), random.uniform(-10, 10)), 'XYZ')
+            body.pose.bones["chest"].rotation_mode   = 'XYZ'
+            body.pose.bones["chest"].rotation_euler  = mathutils.Euler((random.uniform(-10, 10), random.uniform(-10, 10), random.uniform(-10, 10)), 'XYZ')
+
+        # random position and scale
+        scale               = random.uniform(0.3, 0.5)
+        body.scale          = [scale, scale, scale]
+        body.location       = [random.uniform(-1.0, 1.0), 0, random.uniform(-0.2, 0.2)]
+        body.rotation_euler = [0, 0, random.uniform(-3.14, 3.14)]
+
 
 def main():
     if not os.path.exists(background_img_dir):
@@ -201,14 +262,14 @@ def main():
     scene.render.resolution_y = resolution_y
     
     # add target model
-    bpy.ops.wm.append(filepath="../models/" + target_model + ".blend", directory="../models/" + target_model + ".blend" + "/Collection/", filename=target_model)
+    bpy.ops.wm.append(filepath="../models/" + blender_file + ".blend", directory="../models/" + blender_file + ".blend" + "/Collection/", filename=target_model)
 
     # check render engine
     if use_gpu:
         scene.render.engine = 'CYCLES'
         scene.cycles.device = 'GPU'
         scene.render.film_transparent = True
-        scene.cycles.samples = 512
+        scene.cycles.samples = 128
 
     
     scene.use_nodes = True
@@ -276,7 +337,7 @@ def main():
             img = cv2.imread(img_path)
             img = cv2.rectangle(img, (int((result[0]-result[2]/2)*resolution_x), int((result[1]-result[3]/2)*resolution_y)), (int((result[0]+result[2]/2)*resolution_x), int((result[1]+result[3]/2)*resolution_y)), (255, 0, 0), thickness=1, lineType=cv2.LINE_4)
             img = cv2.circle(img, (int(result[0] * resolution_x), int(result[1] * resolution_y)), 10, (0, 0, 255))
-            # cv2.imwrite(filename= out_debug_dir + str(i + dataset_offset).zfill(digit) + ".jpg", img=img)
+            cv2.imwrite(filename= out_debug_dir + str(i + dataset_offset).zfill(digit) + ".jpg", img=img)
             cv2.imwrite(filename="out.jpg", img=img)
         
         flabel = open(out_label_dir + str(i + dataset_offset).zfill(10) + ".txt", 'w', encoding='UTF-8')
